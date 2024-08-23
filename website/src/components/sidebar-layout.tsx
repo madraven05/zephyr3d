c4f9f908cc41bc3c1575aa0750b88dad7e7c5137
pranshu-kumar <pranshukumargond05@gmail.com>
2024-08-23 22:09:55 +0530

    side bar created

---
+++ website/src/components/sidebar-layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import Sidebar from "./sidebar";
+
+/**
+ *
+ * The sidebar layout that will be used by all the pages
+ * in the documentation
+ */
+const SidebarLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="lg:ml-72 px-10 py-8 flex-grow">{children}</main>
+    </div>
+  );
+};
+
+export default SidebarLayout;
---
+++ website/src/components/sidebar.tsx
@@ -0,0 +1,111 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { HTMLAttributes } from "react";
+
+export type NavChildItem = {
+  title: string;
+  link: string;
+};
+
+export type NavItem = {
+  category: string;
+  navItems: NavChildItem[];
+};
+
+export type SidebarNavItems = {
+  [navItem: string]: NavItem[];
+};
+
+interface SidebarProps {}
+
+const Sidebar: React.FC<SidebarProps> = () => {
+  const router = useRouter();
+  const pageName = router.pathname.split("/").filter(Boolean).pop();
+
+  const sidebarNavItems: SidebarNavItems = {
+    components: [
+      {
+        category: "Data Display",
+        navItems: [
+          {
+            title: "Product Card",
+            link: "/docs/components/",
+          },
+          {
+            title: "Particled Mesh",
+            link: "/docs/components/",
+          },
+          {
+            title: "Particled Mesh",
+            link: "/docs/components/",
+          },
+        ],
+      },
+      {
+        category: "Feedback",
+        navItems: [
+          {
+            title: "Product Card",
+            link: "/docs/components/",
+          },
+          {
+            title: "Particled Mesh",
+            link: "/docs/components/",
+          },
+          {
+            title: "Particled Mesh",
+            link: "/docs/components/",
+          },
+        ],
+      },
+    ],
+    dev: [
+      {
+        category: "Basic Usage",
+        navItems: [
+          {
+            title: "Getting Started",
+            link: "/dev/",
+          },
+        ],
+      },
+      {
+        category: "Other",
+        navItems: [
+          {
+            title: "FAQ",
+            link: "/dev/",
+          },
+          {
+            title: "Contributing",
+            link: "/dev/",
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <aside className="sm:hidden lg:block hidden fixed w-72 h-screen overflow-auto py-8 px-7 bg-slate-800/10 shadow-lg rounded-md text-white font-heading uppercase">
+      {/* side nav */}
+      <ul className="flex flex-col gap-3">
+        {sidebarNavItems[pageName as string].map((item) => (
+          <>
+            <p className="text-gray-300 font-heading">{item.category}</p>
+            <div>
+              {item.navItems.map((n) => (
+                <li className="mb-2">
+                  <Link href={n.link}>{n.title}</Link>
+                </li>
+              ))}
+            </div>
+            <div className="w-full h-0.5 bg-white/10"></div>
+          </>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
