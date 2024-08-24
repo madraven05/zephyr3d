6ed5af210d335f56d98b7c427781bd8e3ed496fa
pranshu-kumar <pranshukumargond05@gmail.com>
2024-08-24 15:46:10 +0530

    dark mode functionality added

--- website/src/components/navbar.tsx
+++ website/src/components/navbar.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Switch } from "@headlessui/react";
+import { MdOutlineWbSunny, MdOutlineDarkMode } from "react-icons/md";
 
 const Navbar = () => {
   const navbarItems = [
@@ -13,11 +15,47 @@ const Navbar = () => {
       link: "/docs/components",
     },
   ];
+
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+
+  useEffect(() => {
+    if (darkModeEnabled) {
+      document.documentElement.classList.toggle("dark");
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+  }, [darkModeEnabled]);
+
   return (
     <nav className="flex justify-center items-center gap-10 text-white font-heading uppercase">
-        {navbarItems.map(item => (
-            <Link href={item.link} className="text-lg hover:font-bold hover:-translate-y-1 hover:cursor-pointer transition duration-300 ease-in-out">{item.title}</Link>
-        ))}
+      {/* Dark mode toggle */}
+      <div className="flex justify-center items-center gap-2">
+        {!darkModeEnabled ? (
+          <MdOutlineWbSunny className="text-xl" />
+        ) : (
+          <MdOutlineDarkMode className="text-xl" />
+        )}
+        <Switch
+          onChange={setDarkModeEnabled}
+          checked={darkModeEnabled}
+          className="group relative flex h-7 w-14 cursor-pointer rounded-full bg-white/10 p-1 transition-colors duration-200 ease-in-out focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white data-[checked]:bg-white/10"
+        >
+          <span
+            aria-hidden="true"
+            className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-7"
+          />
+        </Switch>
+      </div>
+
+      {/* Nav Items */}
+      {navbarItems.map((item) => (
+        <Link
+          href={item.link}
+          className="text-lg hover:font-bold hover:-translate-y-1 hover:cursor-pointer transition duration-300 ease-in-out"
+        >
+          {item.title}
+        </Link>
+      ))}
     </nav>
   );
 };
--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -25,7 +25,6 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           </SidebarLayout>
         )}
       </div>
-      ;
     </>
   );
 }
