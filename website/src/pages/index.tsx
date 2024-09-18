49e4b2da956b5aa85653caa5ea72e7af8175f4b7
pranshu-kumar <pranshukumargond05@gmail.com>
2024-09-18 21:48:40 +0530

    added title to the home page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/hero";
+import Head from "next/head";
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Zephyr3D</title>
+    </Head>
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero></Hero>
     </main>
+    </>
   );
 }
